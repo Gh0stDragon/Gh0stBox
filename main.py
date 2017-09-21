cf36b0a878b3c2f67b367e85cb9017f0c6ab2b85
Gh0stDragon <32177069+Gh0stDragon@users.noreply.github.com>
2017-09-21 21:26:14 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,17 @@
+'''
+    Dev : Gh0st_Dragon
+    Data: 2017
+    Tool: Gh0st_ToolBox
+
+'''
+
+
+print("*********************")
+print("|   Gh0st_Dragon   |*")
+print("|                  |*")
+print("|      ToolBox     |*")
+print("|   Version 0.0.1  |*")
+print("*********************")
+
+print("Enter Number for Choose Tool : ")
+
